c66e38b59bdf2dab140f29977c4c99e2985a9821
ibrahimkhennouf <80972384+ibrahimkhennouf@users.noreply.github.com>
2023-09-12 12:42:32 +0100

    add case sens

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,14 @@
 export const filterPok = (pokList, name, val) => {
   switch (name) {
     case 'name':
-      return pokList.filter((pok) => pok.name.includes(val));
+      return pokList.filter((pok) =>
+        pok.name.toLowerCase().includes(val.toString().toLowerCase())
+      );
 
     case 'type':
-      return pokList.filter((pok) => pok.type.toString().includes(val));
+      return pokList.filter((pok) =>
+        pok.type.toString().toLowerCase().includes(val.toString().toLowerCase())
+      );
 
     default:
       return pokList.filter((pok) => pok[name] >= val);
